69b5aff01f4c219439c17e44b267ca768320499b
Murat Kozan <muratkozan350@gmail.com>
2018-11-25 02:19:39 +0300

    kuntao: Add miracast properties

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -71,6 +71,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.wfd.nohdcp=1 \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0 \
     persist.hwc.enable_vds=1 \
     debug.sf.hw=0 \
     debug.egl.hw=0 \
